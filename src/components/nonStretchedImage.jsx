b3988185e7078f13b25be0b6b7250ac561bb3995
Caught Varyat <adam.dabrowski@outlook.com>
2021-05-25 07:19:48 +0200

    Partial fix for lightbox image

--- src/components/nonStretchedImage.jsx
+++ src/components/nonStretchedImage.jsx
@@ -2,22 +2,23 @@ import React from 'react';
 import Image from 'gatsby-image';
 
 const NonStretchedImage = (props) => {
-	let normalizedProps = props;
-	if (props.fluid && props.fluid.presentationWidth) {
-		normalizedProps = {
-			...props,
-			style: {
-				...(props.style || {}),
-				maxWidth: props.fluid.presentationWidth,
-				// maxHeight: window.innerHeight - 50px,
-				height: window.innerHeight - 50,
+  let normalizedProps = props;
+  if (props.fluid && props.fluid.presentationWidth) {
+    normalizedProps = {
+      ...props,
+      imgStyle: { objectFit: "contain" },
+      style: {
+        ...(props.style || {}),
+//maxWidth: props.fluid.presentationWidth,
+        //maxHeight: window.innerHeight - 50px,
+//height: window.innerHeight - 50,
         maxHeight: '90vh',
-				width: 'auto',
-				margin: '50px auto 0', // Used to center the image
-			},
-		};
-	}
+        maxWidth: '90vw',
+        margin: 'auto', // Used to center the image
+      },
+    };
+  }
 
-	return <Image {...normalizedProps} />;
+  return <Image {...normalizedProps} />;
 };
 export default NonStretchedImage;
