b745c61e2591d231a23601ddb5bfc343dc0efccc
a-dabrowski <adam.dabrowski@outlook.com>
2021-12-22 17:22:12 +0100

    Button variant migrate

---
+++ src/components/buttons/Buttons.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const Button = ({ variant, children }) => {
+  switch (variant) {
+    case 'elevated-1':
+      return (
+        <button className="relative px-6 py-3 font-bold text-black group w-fit">
+          <span className="absolute inset-0 w-full h-full transition duration-300 ease-out transform -translate-x-2 -translate-y-2 bg-red-300 group-hover:translate-x-0 group-hover:translate-y-0"></span>
+          <span className="absolute inset-0 w-full h-full border-4 border-black"></span>
+          <span className="relative">{children}</span>
+        </button>
+      );
+    case 'elevated-2':
+      return (
+        <button className="relative inline-block px-4 py-2 font-medium group w-fit">
+          <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+          <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
+          <span className="relative text-black group-hover:text-white">
+            {children}
+          </span>
+        </button>
+      );
+    case 'elevated-3':
+      return (
+        <button className="relative inline-flex items-center justify-center px-6 py-3 text-lg font-medium tracking-tighter text-white bg-gray-800 rounded-md group w-fit">
+          <span className="absolute inset-0 w-full h-full mt-1 ml-1 transition-all duration-300 ease-in-out bg-purple-600 rounded-md group-hover:mt-0 group-hover:ml-0"></span>
+          <span className="absolute inset-0 w-full h-full bg-white rounded-md "></span>
+          <span className="absolute inset-0 w-full h-full transition-all duration-200 ease-in-out delay-100 bg-purple-600 rounded-md opacity-0 group-hover:opacity-100 "></span>
+          <span className="relative text-purple-600 transition-colors duration-200 ease-in-out delay-100 group-hover:text-white">
+            {children}
+          </span>
+        </button>
+      );
+    case 'elevated-4':
+      return (
+        <button className="relative inline-block text-lg group w-fit">
+          <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+            <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+            <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+            <span className="relative">{children}</span>
+          </span>
+          <span
+            className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+            data-rounded="rounded-lg"
+          ></span>
+        </button>
+      );
+    default:
+      return <button>test</button>;
+  }
+};
+
+export default Button;
--- src/components/buttons/index.js
+++ src/components/buttons/index.js
@@ -1,4 +1,5 @@
+import Button from './Buttons';
 import Cta from './Cta';
-import MinimalButton from './MinimalButton';
 import ImageButton from './ImageButton.jsx';
-export {ImageButton, Cta, MinimalButton};
+import MinimalButton from './MinimalButton';
+export { Button, ImageButton, Cta, MinimalButton };
