bb7baee60d8c477221228aef7c096c4a40da930e
Adam Dabrowski <adam.dabrowski@outlook.com>
2022-11-10 12:05:52 +0100

    WIP upgrade to latest gatsby

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -40,27 +40,25 @@ const BlogIndex = ({ data, location }) => {
 
 export default BlogIndex;
 
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
+export const pageQuery = graphql`{
+  site {
+    siteMetadata {
+      title
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-          }
+  }
+  allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
+    edges {
+      node {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
         }
       }
     }
   }
-`;
+}`;
